7b9ad5aeeb17d12f36abaca7558bfebdd5f8aeb2
Tim Ha <thjk92@gmail.com>
2021-02-17 08:32:58 -0800

    setup express route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import express from 'express';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+app.use('/posts', postRoutes);
 
 const URL =
 	'mongodb+srv://admin:dune47kafka@aws-oregon.ldmhj.mongodb.net/<dbname>?retryWrites=true&w=majority';
