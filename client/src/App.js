4d3320b4739d1b4ac89cbb6f037cd19274759866
Tim Ha <thjk92@gmail.com>
2021-02-17 09:04:23 -0800

    Create client skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import captureImg from './images/capture.png';
 
 const App = () => {
 	return (
-		<div>
-			<h1>App</h1>
-		</div>
+		<Container maxWidth="lg">
+			<AppBar position="static" color="inherit">
+				<Typography variant="h2" align="center">
+					Capture
+					<img src={captureImg} alt="capture" height="60" />
+				</Typography>
+			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid
+						container
+						justify="space-between"
+						alignItems="stretch"
+						spacing={3}
+					>
+						<Grid item xs={12} sm={7}>
+							<Posts />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
 	);
 };
 
