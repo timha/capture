7ac9fc85dde830bb277005d3ff40df7407d11b9c
Tim Ha <thjk92@gmail.com>
2021-02-17 08:40:27 -0800

    Create Express controllers for routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,7 @@
 import express from 'express';
-
+import { getPosts } from '../controllers/posts.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('success');
-});
+router.get('/', getPosts);
 
 export default router;
